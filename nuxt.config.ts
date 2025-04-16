363494d8a0ca5b11d046d96f9995c6cb0c9f12b3
MarkSky <rdmark@outlook.com>
2025-04-16 18:03:50 +0800

    [Setting] Nuxt設定檔

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
+    modules: [
+        '@nuxt/content',
+        '@nuxt/eslint',
+        '@nuxt/fonts',
+        '@nuxt/icon',
+        '@nuxt/image',
+        '@nuxt/scripts',
+        '@nuxt/test-utils',
+        '@nuxtjs/i18n',
+    ],
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-  ],
-  devtools: { enabled: true }, compatibilityDate: '2024-11-01',
+    devtools: { enabled: true }, compatibilityDate: '2024-11-01',
 
-  eslint: {
-    config: {
-      stylistic: true,
+    eslint: {
+        config: {
+            stylistic: true,
+        },
     },
-  },
-})
+
+    i18n: {},
+});
